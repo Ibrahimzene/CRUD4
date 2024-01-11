3bd4aa72823fbd41c641b7bea7faa8b9e014e3e2
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-01-11 11:21:47 +0100

    Refactor router configuration in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -12,29 +12,31 @@ import { Page404 } from "./pages/Page404.tsx";
 import { AppProvider } from "./AppContext.tsx";
 import { PageManageFlashcards } from "./pages/PageManageFlashcards.tsx";
 
+const children = [
+	{
+		path: "/welcome",
+		element: <PageWelcome />,
+	},
+	{
+		path: "manage-flashcards",
+		element: <PageManageFlashcards />,
+	},
+	{
+		path: "about",
+		element: <PageAbout />,
+	},
+	{
+		path: "/",
+		element: <Navigate to="/welcome" replace />,
+	},
+];
+
 const router = createBrowserRouter([
 	{
 		path: "/",
 		errorElement: <Page404 />,
 		element: <App />,
-		children: [
-			{
-				path: "/welcome",
-				element: <PageWelcome />,
-			},
-			{
-				path: "manage-flashcards",
-				element: <PageManageFlashcards />,
-			},
-			{
-				path: "about",
-				element: <PageAbout />,
-			},
-			{
-				path: "/",
-				element: <Navigate to="/welcome" replace />,
-			},
-		],
+		children,
 	},
 ]);
 
