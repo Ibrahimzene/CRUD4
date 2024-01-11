bfbe35b96230f0999a48ba92483443a90e780b65
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-01-11 11:19:57 +0100

    Refactor Nav component to conditionally render Manage Flashcards link based on siteEnvironment

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,13 +5,23 @@ import { AppContext } from "../AppContext";
 export const Nav = () => {
 	const { siteEnvironment } = useContext(AppContext);
 
-	return <nav>
-		<ul className="flex gap-4 bg-slate-500 px-4 py-2 content">
-			<li><NavLink to="/welcome">Welcome</NavLink></li>
-			{siteEnvi}
-			<li><NavLink to="/manage-flashcards">Manage Flashcards</NavLink></li>
-			)}
-			<li><NavLink to="/about">About</NavLink></li>
-		</ul>
-	</nav>;
+	return (
+		<nav>
+			<ul className="flex gap-4 bg-slate-500 px-4 py-2 content">
+				<li>
+					<NavLink to="/welcome">Welcome</NavLink>
+				</li>
+				{siteEnvironment === "development" && (
+					<li>
+						<NavLink to="/manage-flashcards">
+							Manage Flashcards
+						</NavLink>
+					</li>
+				)}
+				<li>
+					<NavLink to="/about">About</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
 };
